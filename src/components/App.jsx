aed0103203dbbb22f65a5b8adaf23cf3b47d46df
Leonard392 <leorp392@gmail.com>
2024-01-23 12:01:32 -0500

    create notes.js and linkeded it

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom";
 import Heading from "./Header";
 import Footing from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+
+
+
 
 function App() {
     return (<div>
         <Heading />
-        <Note />
+        {notes.map((noteItem) => <Note
+                key = {noteItem.key}
+                title = {noteItem.title}
+                content = {noteItem.content}
+              />
+         )}
         <Footing />
     </div>
     )
